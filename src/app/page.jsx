eb9f1890c8ee1beac5224b343cb94a31adb98ea5
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2023-08-16 23:40:27 +0500

    Tailwind Setup, home page

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+
+import Calculator from '../../components/Calculator'
+export default function Home() {
+	return (
+		<>
+			<div className="text-xl flex justify-center" >
+				<h2 className=''>Scientific Calculator</h2>
+			</div>
+			<Calculator />
+		</>
+	);
+}
